1a8937b4c26ae97314214aa1a5256d824e891d8e
Saad Waseem <saadwaseem@addo.ai>
2025-01-16 18:48:25 +0500

    cleaner implemented completely, verify for edge cases

---
+++ cleaner.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import re
+import numpy as np
+
+def create_cleaned_table(data, year_col):
+    df = data['table'].copy(deep=True)
+
+    # Fix column names
+    df.iloc[0].ffill(inplace=True, axis=0)
+    if df.iloc[1, 0] is None:
+        df.at[0, 0], df.at[1, 0] = df.iloc[0, 0].split(' /\n')
+    if data['unit'] == '':
+        df.at[0, 0], df.at[1, 0] = df.iloc[1, 0], df.iloc[0, 0]
+    df.at[0, 0] = df.iloc[0, 0].upper().strip()
+    df.at[1, 0] = df.iloc[1, 0].upper().strip()
+    df.columns = pd.MultiIndex.from_arrays([df.iloc[0], df.iloc[1]])
+    df = df.drop([0, 1]).reset_index(drop=True)
+    df = df.replace('', np.nan)
+    df.dropna(axis=0, how='all', inplace=True)
+    if data['unit'] == '':
+        df.columns = df.columns.swaplevel(0, 1)
+        df = df[['DIVISIONS', year_col]]
+        df.columns = df.columns.droplevel(1)
+    else:
+        df = df[['DIVISIONS', year_col]]
+        df.columns = df.columns.droplevel(0)
+    df = df[~(df['DISTRICTS'].str.upper().str.contains('PUNJAB'))].reset_index(drop=True)
+
+    # Add divisions
+    df['DIVISIONS'] = ''
+    last_div = ''
+    for ix, row in df.iterrows():
+        district_val = row['DISTRICTS']
+        extract = re.search(r'(.*) D.*V:', district_val, flags = re.IGNORECASE)
+        if extract:
+            last_div = extract.group(1)
+            df.loc[ix, 'DIVISIONS'] = extract.group(1)
+            df.drop(index=ix, axis=0, inplace=True)
+            continue
+        df.loc[ix, 'DIVISIONS'] = last_div
+    df['DIVISIONS'] = df['DIVISIONS'].map(lambda x: x.title())
+
+    # Clean Table for storage
+    col_list = df.columns.tolist()
+    df = pd.melt(df, id_vars=[col_list[0], col_list[-1]], var_name='MEASURE_TYPE', value_name='MEASURE_VALUE')
+    df['TITLE'] = data['heading']
+    df['YEAR'] = int(year_col.split('-')[0])
+
+    # Add unit value
+    if data['unit'] == '':
+        df['UNIT'] = ''
+        for ix, row in df.iterrows():
+            row_val = row['MEASURE_TYPE']
+            df.loc[ix, 'MEASURE_TYPE'], df.loc[ix, 'UNIT'] = re.search(r'(.*)\((.*)\)', row_val).groups()
+
+    else:
+        df['UNIT'] = data['unit']
+    def convert_to_numeric(val):
+        try:
+            return str(float(val))
+        except ValueError:
+            return 'NOVAL'
+    df['MEASURE_VALUE'] = df['MEASURE_VALUE'].apply(convert_to_numeric)
+    df = df[df['MEASURE_VALUE'] != 'NOVAL']
+    df['MEASURE_VALUE'] = df['MEASURE_VALUE'].astype(np.float32)
+    return df[['YEAR', 'TITLE', 'DIVISIONS', 'DISTRICTS', 'MEASURE_TYPE', 'UNIT', 'MEASURE_VALUE']].reset_index()
