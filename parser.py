aeffadfabd5e95e1127c2a77009cfc8b008fe6ec
Saad Waseem <saadwaseem@addo.ai>
2025-01-16 12:22:14 +0500

    Fixed edge cases

--- parser.py
+++ parser.py
@@ -3,7 +3,26 @@ import pandas as pd
 import re
 from tqdm import tqdm
 
-class PDFParser:
+
+class DataStore:
+        def __init__(self, data=[]):
+            self.parsed_data = data
+
+        def __len__(self):
+            return len(self.parsed_data)
+        
+        def __getitem__(self, ix):
+            return self.parsed_data[ix]
+        
+        def append(self, data):
+            data = {
+                'heading': data[0][0],
+                'unit': data[0][1],
+                'table': data[1]
+            }
+            self.parsed_data.append(data)
+
+class PDFParser:        
     def __init__(self, file_path):
         self.file_path = file_path
 
@@ -11,32 +30,33 @@ class PDFParser:
         page_text = page.extract_text()
         try:
             heading = re.search(r'FINAL ESTIMATE OF(.*?)\n', page_text, re.DOTALL).group(1).strip() if page_text else ''
-            unit = re.search(r'Crop Reporting Service, Punjab(.*?)\n', page_text, re.DOTALL).group(1).strip() if page_text else ''
+            unit_search = re.search(r'\((Avg\..*|Production.*|Area.*)\)', page_text, re.DOTALL) if page_text else ''
+            unit = unit_search.group(1).strip() if unit_search else ''
         except AttributeError as e:
             return None
-        return {'heading': heading, 'unit': unit}
+        return None if heading == '' else (heading, unit)
 
     def get_tables(self, page):
         tables = page.extract_tables()
         return pd.DataFrame(tables[0])
 
     def parse_pdf(self):
-        data = []
+        data = DataStore()
         print('Reading Pages: ')
         with pdfplumber.open(self.file_path) as pdf:
             for page in tqdm(pdf.pages):
                 type_metric_col = self.get_text(page)
                 if type_metric_col is None:
                     continue
-                tables = self.get_tables(page)
-                data.append({'text': type_metric_col, 'tables': tables})
+                table = self.get_tables(page)
+                data.append((type_metric_col, table))
             print('Pages Read Successfully!')
         return data
 
 # Usage
 if __name__ == '__main__':
-    file_path = 'pdf_files/kharif_links_2020-21.pdf'
+    file_path = 'pdf_files/kharif_links_2023-24.pdf'
     parser = PDFParser(file_path)
     parsed_data = parser.parse_pdf()
-    print(parsed_data[0])
 
+    print(parsed_data[-5])
